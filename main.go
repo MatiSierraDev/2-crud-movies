d08ca2abc80684104afa57d5381c801b1730843d
Matias Sierra <matisierra.develop@gmail.com>
2023-05-25 16:13:59 -0300

    Add api rest

--- main.go
+++ main.go
@@ -4,99 +4,138 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 // estructura de las peliculas
 type Movies struct {
+	ID       string `json:"id"`
 	Nombre   string `json:"nombre"`
-	Isbn     string `json:"isbn"`
+	Release  string `json:"release"`
 	Director string `json:"director"`
 }
+
+const serverPort = 3030
 
 var movies []Movies
 
 // funciones para manejar las peticiones
 func GetAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
 	if r.URL.Path != "/movies" {
 		http.Error(w, "Error bad request", http.StatusNotFound)
+		return
 	}
 
 	if r.Method != "GET" {
 		http.Error(w, "Error Method not supported", http.StatusNotFound)
+		return
 	}
 
-	fmt.Fprintf(w, "%s\n", r.URL.Path)
-	fmt.Fprintf(w, "%s\n", r.Method)
+	json.NewEncoder(w).Encode(movies)
 }
 
 func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.URL.Path != "/movies" {
+		http.Error(w, "Bad request", http.StatusNotFound)
+		return
+	}
 
 	if r.Method != "POST" {
 		http.Error(w, "Error bad Request", http.StatusNotFound)
+		return
 	}
 
 	var movie Movies
+
 	json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+
+	for _, item := range movies {
+		if movie.ID == item.ID {
+			http.Error(w, "Bad Request", http.StatusConflict)
+			return
+		}
+	}
 
 	movies = append(movies, movie)
 
-	fmt.Fprintf(w, "%s\n", r.URL.Path)
-	fmt.Fprintf(w, "%s\n", r.Method)
-	fmt.Fprintf(w, "%v\n", movie)
 	fmt.Fprintf(w, "peliculas: %v\n", movies)
 }
 
-func DeleteMovie(w http.ResponseWriter, r *http.Request) {}
+func DeleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
-func UpdateMovie(w http.ResponseWriter, r *http.Request) {}
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func UpdateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movies
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
 
 func GetMovie(w http.ResponseWriter, r *http.Request) {
-	idq := r.URL.Query().Get("id")
-	fmt.Println(idq)
+	w.Header().Set("Content-Type", "application/json")
+
+	dataId := mux.Vars(r)["id"]
+
+	for _, movie := range movies {
+		if movie.ID == dataId {
+			json.NewEncoder(w).Encode(movie)
+			fmt.Println(movie)
+			return
+		}
+	}
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-
 	path := r.URL.Path
 	method := r.Method
 
-	if path != "/home" {
-		http.Error(w, "Error bad request", http.StatusNotFound)
+	if path != "/" {
+		http.Error(w, "Error bad request index", http.StatusNotFound)
 	}
 
 	if method != "GET" {
 		http.Error(w, "Error Method not supported.", http.StatusNotFound)
 	}
 
-	fmt.Fprintf(w, "PATH: %s\n", path)
-	fmt.Fprintf(w, "METHOD: %s\n", method)
-
 	w.Write([]byte(path))
 }
 
 func main() {
-
-	// create server
-	// fmt.Println("Listening on port 3000")
-
-	//serverMux para manejar multiples solicitudes
-	// muxServe := http.NewServeMux()
-
-	//endPoints
-	// muxServe.HandleFunc("/home", Index)
-	// muxServe.HandleFunc("/movies/get", GetAllMovies)
-	// muxServe.HandleFunc("/movies/get/:id", GetMovie)
-	// muxServe.HandleFunc("/movies/post", CreateMovie)
-	// muxServe.HandleFunc("/movies/delete/:id", DeleteMovie)
-	// muxServe.HandleFunc("/movies/update/:id", UpdateMovie)
-
 	//ENRUTADOR GORILLA/MUX
 	mux := mux.NewRouter()
 
+	movies = append(movies, Movies{ID: "1", Nombre: "Dragon Ball: The Legend of Shen Long", Release: "December 20, 1986", Director: "Daisuke Nishio"})
+	movies = append(movies, Movies{ID: "2", Nombre: "Dragon Ball: Sleeping Princess in Devil's Castle", Release: "July 18, 1987", Director: "Daisuke Nishio"})
 	mux.HandleFunc("/", Index).Methods("GET")
 	mux.HandleFunc("/movies", GetAllMovies).Methods("GET")
 	mux.HandleFunc("/movies/{id}", GetMovie).Methods("GET")
@@ -104,7 +143,14 @@ func main() {
 	mux.HandleFunc("/movies/{id}", DeleteMovie).Methods("DELETE")
 	mux.HandleFunc("/movies/{id}", UpdateMovie).Methods("PUT")
 
-	if err := http.ListenAndServe("127.0.0.1:3000", nil); err != nil {
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", serverPort),
+		Handler: mux,
+	}
+
+	fmt.Printf("Listening on server %d ", serverPort)
+
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 
